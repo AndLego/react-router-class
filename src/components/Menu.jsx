b3ba5c00e05f5eadbbd5b00807f2a78dc8d653c8
AndLego <galeon.gb@gmail.com>
2022-12-20 19:33:32 -0500

    dynamic menu, hide link if the user is logged or not

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,31 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../utils/auth";
 import { routes } from "./routes";
 
 const Menu = () => {
+  const { user } = useAuth();
+
   return (
     <nav>
       <ul>
-        {routes.map((ruta) => (
-          <li key={ruta.id}>
-            <NavLink
-              key={ruta.id}
-              to={ruta.to}
-              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
-            >
-              {ruta.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map((ruta) => {
+          if (ruta.publicOnly && user) return null;
+          if (ruta.private && !user) return null;
+
+          return (
+            <li key={ruta.id}>
+              <NavLink
+                key={ruta.id}
+                to={ruta.to}
+                style={({ isActive }) => ({
+                  color: isActive ? "red" : "blue",
+                })}
+              >
+                {ruta.text}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
--- src/components/routes.js
+++ src/components/routes.js
@@ -3,25 +3,31 @@ export const routes = [
     id: 1,
     to: "/",
     text: "Home",
+    private: false,
   },
   {
     id: 2,
     to: "/blog",
     text: "Blog",
+    private: false,
   },
   {
     id: 3,
     to: "/profile",
     text: "Profile",
+    private: true,
   },
   {
     id: 4,
     to: "/logIn",
     text: "LogIn",
+    private: false,
+    publicOnly: true,
   },
   {
     id: 5,
     to: "/logOut",
     text: "LogOut",
+    private: true,
   },
 ];
